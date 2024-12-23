7e6f8d5f9b06bb6d3bbabf98e663385415c1f2b9
Nazar <nazarpanchenko1997@gmail.com>
2024-12-23 23:18:59 +0100

    add translation questions

--- script.js
+++ script.js
@@ -31,97 +31,97 @@ const categories = [
     { 
         name: "History", 
         levels: [
-            { level: "Easy", question: "Who was the first president of the United States?", translation: "Де живе Санта Клаус?", answer: "George Washington", used: false },
-            { level: "Medium", question: "How many years did the Hundred Years' War last?", answer: "116 years (from 1337 to 1453)", used: false },
-            { level: "Hard", question: "Which country was the first to grant women the right to vote in 1893?", answer: "New Zealand", used: false }
+            { level: "Easy", question: "Who was the first president of the United States?", translation: "Wer war der erste Präsident der Vereinigten Staaten?", answer: "George Washington", used: false },
+            { level: "Medium", question: "How many years did the Hundred Years' War last?", translation: "Wie viele Jahre dauerte der Hundertjährige Krieg?", answer: "116 years (from 1337 to 1453)", used: false },
+            { level: "Hard", question: "Which country was the first to grant women the right to vote in 1893?", translation: "Welches Land war das erste, das 1893 den Frauen das Wahlrecht gewährte?", answer: "New Zealand", used: false }
         ]
     },
     { 
         name: "Sport",
         levels: [
-            { level: "Easy", question: "Which tennis player has the most Grand Slam titles among men (as of 2024)?", answer: "Novak Djokovic", used: false },
-            { level: "Medium", question: "Which country hosted the first FIFA World Cup?", answer: "Uruguay (1930)", used: false },
-            { level: "Hard", question: "Which country is always the first to enter the parade of participants during the opening ceremony of the Olympic Games, and which country ends the parade?", answer: "The first is Greece, the last is the host country", used: false }
+            { level: "Easy", question: "Which tennis player has the most Grand Slam titles among men (as of 2024)?", translation: "Welcher Tennisspieler hat die meisten Grand-Slam-Titel bei den Männern (ab 2024)?", answer: "Novak Djokovic", used: false },
+            { level: "Medium", question: "Which country hosted the first FIFA World Cup?", translation: "Welches Land war Gastgeber der ersten FIFA-Weltmeisterschaft?", answer: "Uruguay (1930)", used: false },
+            { level: "Hard", question: "Which country is always the first to enter the parade of participants during the opening ceremony of the Olympic Games, and which country ends the parade?", translation: "Welches Land nimmt bei der Eröffnungsfeier der Olympischen Spiele immer als erstes an der Parade der Teilnehmer teil, und welches Land beendet die Parade?", answer: "The first is Greece, the last is the host country", used: false }
         ]
     },
     { 
         name: "Space",
         levels: [
-            { level: "Easy", question: "How many planets are there in the solar system?", answer: "8", used: false },
-            { level: "Medium", question: "What is the largest object in the solar system after the Sun?", answer: "Jupiter", used: false },
-            { level: "Hard", question: "What is the name of the largest volcano in the solar system, which is located on Mars?", answer: "Olympus Mons", used: false }
+            { level: "Easy", question: "How many planets are there in the solar system?", translation: "Wie viele Planeten gibt es in unserem Sonnensystem?", answer: "8", used: false },
+            { level: "Medium", question: "What is the largest object in the solar system after the Sun?", translation: "Welches ist das größte Objekt im Sonnensystem nach der Sonne?", answer: "Jupiter", used: false },
+            { level: "Hard", question: "What is the name of the largest volcano in the solar system, which is located on Mars?", translation: "Wie heißt der größte Vulkan im Sonnensystem, der sich auf dem Mars befindet?", answer: "Olympus Mons", used: false }
         ]
     },
     { 
         name: "Food",
         levels: [
-            { level: "Easy", question: "What is the most popular drink in the world after water?", answer: "Tea", used: false },
-            { level: "Medium", question: "What kind of delicacy mushroom is grown underground and searched for with the help of dogs or pigs?", answer: "Truffle", used: false },
-            { level: "Hard", question: "Which country is the birthplace of crispy croissants?", answer: "Austria", used: false }
+            { level: "Easy", question: "What is the most popular drink in the world after water?", translation: "Welches ist nach Wasser das beliebteste Getränk der Welt?", answer: "Tea", used: false },
+            { level: "Medium", question: "What kind of delicacy mushroom is grown underground and searched for with the help of dogs or pigs?", translation: "Welche Art von Delikatesspilz wird unterirdisch gezüchtet und mit Hilfe von Hunden oder Schweinen gesucht?", answer: "Truffle", used: false },
+            { level: "Hard", question: "Which country is the birthplace of crispy croissants?", translation: "Which country is the birthplace of crispy croissants?", answer: "Austria", used: false }
         ]
     },
     { 
         name: "Fashion and style",
         levels: [
-            { level: "Easy", question: "What is the name of the fabric that is the basis for jeans?", answer: "Denim", used: false },
-            { level: "Medium", question: "Which designer created the famous “little black dress”?", answer: "Coco Chanel", used: false },
-            { level: "Hard", question: "Who is the most famous male face of Dior Sauvage?", answer: "Johnny Depp", used: false }
+            { level: "Easy", question: "What is the name of the fabric that is the basis for jeans?", translation: "Wie heißt der Stoff, aus dem die Jeans hergestellt wird?", answer: "Denim", used: false },
+            { level: "Medium", question: "Which designer created the famous “little black dress”?", translation: "Welcher Designer hat das berühmte „kleine Schwarze“ entworfen?", answer: "Coco Chanel", used: false },
+            { level: "Hard", question: "Who is the most famous male face of Dior Sauvage?", translation: "Wer ist das berühmteste männliche Gesicht von Dior Sauvage?", answer: "Johnny Depp", used: false }
         ]
     },
     { 
         name: "Netflix series",
         levels: [
-            { level: "Easy", question: "Which Netflix series tells the story of paranormal events in the town of Hawkins in the 1980s?", answer: "Stranger Things", used: false },
-            { level: "Medium", question: "Which Netflix series has become the most successful in the South Korean drama genre?", answer: "Squid Game", used: false },
-            { level: "Hard", question: "Which Netflix series tells the story of a British criminal group in the early 20th century?", answer: "Peaky Blinders", used: false }
+            { level: "Easy", question: "Which Netflix series tells the story of paranormal events in the town of Hawkins in the 1980s?", translation: "Welche Netflix-Serie erzählt die Geschichte von paranormalen Ereignissen in der Stadt Hawkins in den 1980er Jahren?", answer: "Stranger Things", used: false },
+            { level: "Medium", question: "Which Netflix series has become the most successful in the South Korean drama genre?", translation: "Welche Netflix-Serie ist die erfolgreichste im südkoreanischen Drama-Genre?", answer: "Squid Game", used: false },
+            { level: "Hard", question: "Which Netflix series tells the story of a British criminal group in the early 20th century?", translation: "Welche Netflix-Serie erzählt die Geschichte einer britischen Verbrecherbande im frühen 20. Jahrhundert?", answer: "Peaky Blinders", used: false }
         ]
     },
     { 
         name: "Music",
         levels: [
-            { level: "Easy", question: "Which song and band are considered the most popular in the history of the Eurovision Song Contest?", answer: "Waterloo by ABBA (Sweden, 1974)", used: false },
-            { level: "Medium", question: "What famous music festival in the United States, held in 1969, became a symbol of the hippie era?", answer: "Woodstock", used: false },
-            { level: "Hard", question: "What musical instrument is considered the oldest in the world?", answer: "Flute", used: false }
+            { level: "Easy", question: "Which song and band are considered the most popular in the history of the Eurovision Song Contest?", translation: "Welcher Song und welche Band gelten als die beliebtesten in der Geschichte des Eurovision Song Contest?", answer: "Waterloo by ABBA (Sweden, 1974)", used: false },
+            { level: "Medium", question: "What famous music festival in the United States, held in 1969, became a symbol of the hippie era?", translation: "Welches berühmte Musikfestival in den Vereinigten Staaten, das 1969 stattfand, wurde zum Symbol der Hippie-Ära?", answer: "Woodstock", used: false },
+            { level: "Hard", question: "What musical instrument is considered the oldest in the world?", translation: "Welches Musikinstrument gilt als das älteste der Welt?", answer: "Flute", used: false }
         ]
     },
     { 
         name: "Geography",
         levels: [
-            { level: "Easy", question: "What city is the capital of Australia?", answer: "Canberra", used: false },
-            { level: "Medium", question: "Which country is the smallest in the world by area?", answer: "Vatican City", used: false },
-            { level: "Hard", question: "Which country has the largest number of neighboring countries?", answer: "China (14 neighbors)", used: false }
+            { level: "Easy", question: "What city is the capital of Australia?", translation: "Welche Stadt ist die Hauptstadt von Australien?", answer: "Canberra", used: false },
+            { level: "Medium", question: "Which country is the smallest in the world by area?", translation: "Welches Land ist flächenmäßig das kleinste der Welt?", answer: "Vatican City", used: false },
+            { level: "Hard", question: "Which country has the largest number of neighboring countries?", translation: "Welches Land hat die größte Anzahl von Nachbarländern?", answer: "China (14 neighbors)", used: false }
         ]
     },
     { 
         name: "Movies",
         levels: [
-            { level: "Easy", question: "Which movie is considered the highest-grossing film in the history of cinema?", answer: "Avatar (2009)", used: false },
-            { level: "Medium", question: "Which movie won the Oscar for Best Picture in 2024?  [Which movie won the Oscar for Best Picture in 2024?]",  answer: "Oppenheimer", used: false },
-            { level: "Hard", question: "What movie has the highest rating on IMDb in history as of 2024?", answer: "The Shawshank Redemption (1994)", used: false }
+            { level: "Easy", question: "Which movie is considered the highest-grossing film in the history of cinema?", translation: "Welcher Film gilt als der umsatzstärkste Film in der Geschichte des Kinos?", answer: "Avatar (2009)", used: false },
+            { level: "Medium", question: "Which movie won the Oscar for Best Picture in 2024?", translation: "Welcher Film gewann den Oscar für den besten Film im Jahr 2024?",  answer: "Oppenheimer", used: false },
+            { level: "Hard", question: "What movie has the highest rating on IMDb in history as of 2024?", translation: "Welcher Film hat bis 2024 die höchste Bewertung auf IMDb in der Geschichte?", answer: "The Shawshank Redemption (1994)", used: false }
         ]
     },
     { 
         name: "Christmas",
         levels: [
-            { level: "Easy", question: "What German dessert popular during the Christmas season is shaped like bread with raisins, nuts, and candied fruit?", answer: "Stollen", used: false },
-            { level: "Medium", question: "What city does the legend of St. Nicholas come from?", answer: "Myra, Lycia (modern Turkey)", used: false },
-            { level: "Hard", question: "In which country do people traditionally eat KFC fried chicken for Christmas?", answer: "Japan", used: false }
+            { level: "Easy", question: "What German dessert popular during the Christmas season is shaped like bread with raisins, nuts, and candied fruit?", translation: "Welches deutsche Dessert, das in der Weihnachtszeit beliebt ist, hat die Form eines Brotes mit Rosinen, Nüssen und kandierten Früchten?", answer: "Stollen", used: false },
+            { level: "Medium", question: "What city does the legend of St. Nicholas come from?", translation: "Aus welcher Stadt stammt die Legende von St. Nikolaus?", answer: "Myra, Lycia (modern Turkey)", used: false },
+            { level: "Hard", question: "In which country do people traditionally eat KFC fried chicken for Christmas?", translation: "In welchem Land isst man an Weihnachten traditionell Brathähnchen von KFC?", answer: "Japan", used: false }
         ]
     },
     { 
         name: "Deutschland",
         levels: [
-            { level: "Easy", question: "Which modern German car company is the world's largest car manufacturer by sales?", answer: "Volkswagen Group", used: false },
-            { level: "Medium", question: "Which architectural school founded in 1919 in Germany had a huge impact on modern design?", answer: "Bauhaus", used: false },
-            { level: "Hard", question: "Which city in Germany is the oldest?", answer: "Trier", used: false }
+            { level: "Easy", question: "Which modern German car company is the world's largest car manufacturer by sales?", translation: "Welches moderne deutsche Automobilunternehmen ist der größte Automobilhersteller der Welt, gemessen am Umsatz?", answer: "Volkswagen Group", used: false },
+            { level: "Medium", question: "Which architectural school founded in 1919 in Germany had a huge impact on modern design?", translation: "Welche 1919 in Deutschland gegründete Architekturschule hatte einen großen Einfluss auf das moderne Design?", answer: "Bauhaus", used: false },
+            { level: "Hard", question: "Which city in Germany is the oldest?", translation: "Welche Stadt in Deutschland ist die älteste?", answer: "Trier", used: false }
         ]
     },
     { 
         name: "Celebrities",
         levels: [
-            { level: "Easy", question: "Who is the most popular person on Instagram by number of followers?", answer: "Cristiano Ronaldo", used: false },
-            { level: "Medium", question: "Which actress became famous for her role as Hermione Granger?", answer: "Emma Watson", used: false },
-            { level: "Hard", question: "Who is the world's youngest billionaire who achieved this status through her own business?", answer: "Kylie Jenner (21 years old)", used: false }
+            { level: "Easy", question: "Who is the most popular person on Instagram by number of followers?", translation: "Wer ist die beliebteste Person auf Instagram nach Anzahl der Follower?", answer: "Cristiano Ronaldo", used: false },
+            { level: "Medium", question: "Which actress became famous for her role as Hermione Granger?", translation: "Welche Schauspielerin wurde durch ihre Rolle als Hermine Granger berühmt?", answer: "Emma Watson", used: false },
+            { level: "Hard", question: "Who is the world's youngest billionaire who achieved this status through her own business?", translation: "Wer ist die jüngste Milliardärin der Welt, die diesen Status durch ihr eigenes Unternehmen erreicht hat?", answer: "Kylie Jenner (21 years old)", used: false }
         ]
     }
 ];
